abf0b78650a9850e18f1616c4056cde9b1ad2bb7
Rishabh <63032657+rishabhbalaji@users.noreply.github.com>
2023-04-20 15:51:18 +0530

    Chat save feature has been added

--- chatgpt.py
+++ chatgpt.py
@@ -1,20 +1,28 @@
-
 import openai
+import psycopg2
 import argparse
 
 openai.api_key = "<OPENAI API KEY HERE>"
 
-def generate_text(prompt):
+def generate_text(prompt, conn):
     model_engine = "text-davinci-002"
     response = openai.Completion.create(
-      engine=model_engine,
-      prompt=prompt,
-      max_tokens=1024,
-      n=1,
-      stop=None,
-      temperature=0.5,
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
     )
-    return response.choices[0].text.strip()
+    generated_text = response.choices[0].text.strip()
+
+    # Save the interaction to the database
+    cur = conn.cursor()
+    cur.execute("INSERT INTO interactions (input, output) VALUES (%s, %s)", (prompt, generated_text))
+    conn.commit()
+    cur.close()
+
+    return generated_text
 
 def main():
     parser = argparse.ArgumentParser(description="CLI interface for ChatGPT")
@@ -23,14 +31,25 @@ def main():
 
     radio_value = args.radio
 
+    # Connect to the database
+    conn = psycopg2.connect(
+        host="your_database_host",
+        database="your_database_name",
+        user="your_database_username",
+        password="your_database_password"
+    )
+
     prompt = f"What is your preferred method of contact? {radio_value}\n"
 
     while True:
         user_input = input("You: ")
         prompt += f"You: {user_input}\n"
-        generated_text = generate_text(prompt)
+        generated_text = generate_text(prompt, conn)
         print(f"ChatGPT: {generated_text}")
         prompt += f"ChatGPT: {generated_text}\n"
+
+    # Close the database connection
+    conn.close()
 
 if __name__ == '__main__':
     main()
